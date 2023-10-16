88fd3cec1a4160b50f7213ba79d784a11690f71d
AnnaRuk <belyaevaan80@gmail.com>
2023-10-16 15:27:14 +0200

    hw_lesson_16

--- src/test/java/org/ait/herokuapp/tests/TestBase.java
+++ src/test/java/org/ait/herokuapp/tests/TestBase.java
@@ -1,25 +1,55 @@
 package org.ait.herokuapp.tests;
 
+import org.ait.herokuapp.utils.MyListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 public class TestBase {
 
+
+   Logger logger = LoggerFactory.getLogger(TestBase.class);
+   String browser = System.getProperty("browser", Browser.CHROME.browserName());
    public WebDriver driver;
 
     @BeforeMethod
-    public void init(){
-        driver = new ChromeDriver();
-        driver.get("http://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void startLogger(Method m){
+        logger.info("*****************************");
+        logger.info("Start method --> " + m.getName());
     }
 
-    @AfterMethod(enabled = false)
+    @BeforeMethod
+    public void init(){
+        //driver = new ChromeDriver();
+        if (browser.equals(Browser.CHROME.browserName())){
+            driver = new ChromeDriver();
+            logger.info("All tests run in Chrome Browser");
+        } else if (browser.equalsIgnoreCase(Browser.FIREFOX.browserName())){
+            driver = new FirefoxDriver();
+            logger.info("All tests run in Firefox Browser");
+        }
+
+        WebDriverListener listener = new MyListener();
+        driver = new EventFiringDecorator(listener).decorate(driver);
+        driver.get("http://the-internet.herokuapp.com/");
+        logger.info("The link -->" + driver.getCurrentUrl());
+        logger.info("***************************************************");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @AfterMethod(enabled = true)
     public void tearDown(){
         driver.quit();
     }
